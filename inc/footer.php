d3060d85a9375237a15e71c29bf377b60556774e
french-femboi <cat@french-femboi.eu>
2024-09-16 20:07:32 +0200

    changed a link on the footer and prepared for the new tailwindcss layout

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <div class="inner-card-footer">
     <p>Copyright © Catpawz - 2024</p>
     <p>Made with 💜 and 🍪 from Catpawz</p>
-    <a class="link" href="/privacy">PRIVACY NOTICE</a> - <a class="link" href="https://network-status.catpawz.eu" target="_blank">NETWORK STATUS</a> - <a class="link" href="https://catpawz.eu" target="_blank">CATPAWZ</a>
+    <a class="link" href="/privacy">PRIVACY NOTICE</a> - <a class="link" href="https://network-status.catpawz.eu" target="_blank">NETWORK STATUS</a> - <a class="link" href="https://french-femboi.eu" target="_blank">CATPAWZ</a>
 </div>
